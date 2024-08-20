bd91a7c787d22626bf6a1ee8373a5889b5766d7d
[nowashrain] <[rltkanth@gmail.com]>
2024-08-20 10:57:01 +0900

    ORM으로 테이블 자동 생성

---
+++ app/model/member.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.model.base import Base
+
+
+class Member(Base):
+    __tablename__ = 'member'
+
+    mno: Mapped[int] = mapped_column(primary_key=True,
+                                     autoincrement=True, index=True)
+
+    userid: Mapped[str] = mapped_column(index=True)
+    passwd: Mapped[str]
+    name: Mapped[str]
+    email: Mapped[str]
+    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
