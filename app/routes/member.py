96f35e64516248454e1abfa545ad08fa949d55c3
[nowashrain] <[rltkanth@gmail.com]>
2024-08-22 12:08:44 +0900

    아이디로 조회환 회원정보를 myinfo.html에 출력

--- app/routes/member.py
+++ app/routes/member.py
@@ -3,32 +3,33 @@ from sqlalchemy.orm import Session
 from starlette.requests import Request
 from starlette.responses import HTMLResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
+
 from app.dbfactory import get_db
 from app.schema.member import NewMember
 from services.member import MemberService
-
 
 
 member_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
 
 
-
-
-
 @member_router.get('/join', response_class=HTMLResponse)
 async def join(req: Request):
     return templates.TemplateResponse('member/join.html', {'request': req})
 
-member_router.post('/join', response_class=HTMLResponse)
+@member_router.post('/join', response_class=HTMLResponse)
 async def joinok(member: NewMember, db: Session = Depends(get_db)):
     try:
-        print(member)
-        result = MemberService.insert_member(db, member)
-        print('처리결과 : ', result.rowcount)
+        if MemberService.check_captcha(member):
+            print(member)
+            result = MemberService.insert_member(db, member)
+            print('처리결과 : ', result.rowcount)
 
-        if result.rowcount > 0:  # 회원가입이 성공적으로 완료되면
-            return RedirectResponse(url='/member/login', status_code=303)
+            if result.rowcount > 0:  # 회원가입이 성공적으로 완료되면
+                return RedirectResponse(url='/member/login', status_code=303)
+        else:
+            return RedirectResponse(url='/member/error', status_code=303)
+
 
     except Exception as ex:
         print(f'▷▷▷ joinok 오류 발생 : {str(ex)}')
@@ -42,21 +43,55 @@ async def login(req: Request):
 
 @member_router.post('/login', response_class=HTMLResponse)
 async def loginok(req: Request, db: Session = Depends(get_db)):
-    data = await req.json() # 클라이언트가 보낸 데이터를 request 객체로 보냄
+    data = await req.json()  # 클라이언트가 보낸 데이터를 request 객체로 받음
     try:
         print('전송한 데이터 : ', data)
-        redirect_url = '/member/loginfail'
+        redirect_url = '/member/loginfail' # 로그인 실패시 loginfail 로 이동
 
-        if MemberService.login_member(db,data):  # 로그인 성공시
+        if MemberService.login_member(db, data): # 로그인 성공시
             req.session['logined_uid'] = data.get('userid')  # 세션에 아이디 저장하고
             redirect_url = '/member/myinfo'  # myinfo 로 이동
 
         return RedirectResponse(url=redirect_url, status_code=303)
+
     except Exception as ex:
         print(f'loginok 오류 : {str(ex)}')
         return RedirectResponse(url='/member/error', status_code=303)
 
 
+@member_router.get('/logout', response_class=HTMLResponse)
+async def error(req: Request):
+    req.session.clear()   # 생성된 세션객체 제거
+    return RedirectResponse('/', status_code=303)
+
+
+
+
+
+
 @member_router.get('/myinfo', response_class=HTMLResponse)
-async def myinfo(req: Request):
-    return templates.TemplateResponse('member/myinfo.html', {'request': req})
+async def myinfo(req: Request, db: Session =Depends(get_db)):
+    try:
+        if 'logined_uid' not in req.session:  # 로그인하지 않았다면
+            return RedirectResponse(url='/member/login', status_code=303)
+
+        # 로그인 했다면 아이디로 회원정보 조회후 myinfo에 출력
+        myinfo = MemberService.selectone_member(db, req.session['logined_uid'])
+        return templates.TemplateResponse('member/myinfo.html', {'request': req, 'myinfo':myinfo})
+
+
+
+    except Exception as ex:
+        print(f'▷▷▷ myinfo 오류 발생 : {str(ex)}')
+        return RedirectResponse(url='/member/error', status_code=303)
+
+
+
+@member_router.get('/error', response_class=HTMLResponse)
+async def error(req: Request):
+    return templates.TemplateResponse('member/error.html', {'request': req})
+
+
+@member_router.get('/loginfail', response_class=HTMLResponse)
+async def error(req: Request):
+    return templates.TemplateResponse('member/loginfail.html', {'request': req})
--- services/member.py
+++ services/member.py
@@ -1,6 +1,6 @@
-from sqlalchemy import insert, select
+import requests
+from sqlalchemy import insert, select, and_
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.sql.operators import and_
 
 from app.model.member import Member
 
@@ -21,8 +21,24 @@ class MemberService:
             print(f'▶▶▶ insert_member 오류발생: {str(ex)}')
             db.rollback()
 
+    # google recaptcha 확인 url
+    # https://www.google.com/recaptcha/api/siteverify?secret=비밀키&response=응답토큰
     @staticmethod
-    def login_member(db,data):
+    def check_captcha(member):
+        print(member)
+        req_url = 'https://www.google.com/recaptcha/api/siteverify'
+        params = { 'secret': '',
+                   'response': member.captcha }
+        res = requests.get(req_url, params=params)
+        result = res.json()
+        print('check => ', result)
+
+        # return result['success']
+        return True
+
+
+    @staticmethod
+    def login_member(db, data):
         try:
             find_login = and_(Member.userid == data.get('userid'),
                               Member.passwd == data.get('passwd'))
@@ -32,6 +48,17 @@ class MemberService:
             return result
 
         except SQLAlchemyError as ex:
-            print(f'▶▶▶ login_member 오류 발생 : {str(ex)} ')
+            print(f'▶▶▶ login_member 오류 발생 : {str(ex)}')
 
 
+    @staticmethod
+    def selectone_member(db, userid):
+        try:
+            find_uid = Member.userid == userid
+            stmt = select(Member).where(find_uid)
+            result = db.execute(stmt).scalars()
+
+            return result
+
+        except SQLAlchemyError as ex:
+            print(f'▶▶▶ selectone_member 오류 발생 : {str(ex)}')
